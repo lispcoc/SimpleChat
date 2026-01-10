88d3e34d026d49b31ab4ebdd8106d5607cd537a7
LISPCoC <fourier0520@gmail.com>
2026-01-10 13:34:21 +0900

    update

--- api/messages.js
+++ api/messages.js
@@ -111,7 +111,7 @@ module.exports = async (req, res) => {
 
         // データベースに保存
         const query = `
-          INSERT INTO rooms (name, description, password, special_keys, created_at, options)
+          INSERT INTO rooms (name, description, password, special_keys, options, created_at)
           VALUES ($1, $2, $3, $4, $5, $6, NOW())
           RETURNING id;
         `
